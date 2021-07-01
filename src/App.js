a2b3f55ce72a2c78ade73627fb349e70dba98c6f
szerszen <szerszenka@gmail.com>
2021-07-01 09:58:29 +0200

    Added default route component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React, {Component} from "react";
 import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import ContinentList from './ContinentList.js';
 import CountryList from './CountryList.js';
+import Home from './Home.js';
 
 class App extends Component {
     render() {
-        const span = <span> whatever your string </span>
         return (
             <BrowserRouter>
                 <div className="container">
-                    <Link to="/continents">CONTINENTS</Link>
                     <Switch>
+                        <Route exact path="/" component={Home}/>
                         <Route exact path="/continents" component={ContinentList}/>
                         <Route exact path="/continents/:code" component={CountryList} />
                     </Switch>
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import {Link} from "react-router-dom";
+import React from "react";
+
+const Home = () => {
+    return (
+        <div>
+            <Link to="/continents">CONTINENTS</Link>
+        </div>
+    )
+}
+
+export default Home;
+
